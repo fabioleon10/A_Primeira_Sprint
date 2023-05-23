8820b9f384d964d5233a36d2b62baa3b92fcf3d3
sprightlytree2 <luccasdaccosta@gmail.com>
2023-05-23 20:29:50 -0300

    Deletando conteúdo da etapa passada

--- Etapa 3/assets/js/scroll.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-    const headerPage = document.querySelector('.nav-page')
-    headerPage.classList.toggle('active-scroll', window.scrollY > 700);
-    
-    })
