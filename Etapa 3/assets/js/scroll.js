72c608434c58edf6cff46ffefb15b9ba33c7627e
Fabio Leon <fsleon93@gmail.com>
2023-05-25 21:04:47 -0300

    active-scroll

--- Etapa 3/assets/js/scroll.js
+++ Etapa 3/assets/js/scroll.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', () => {
     const headerPage = document.querySelector('.nav-page')
-    headerPage.classList.toggle('active-scroll', window.scrollY > 600);
+    headerPage.classList.toggle('active-scroll', window.scrollY > 200);
     
     })
